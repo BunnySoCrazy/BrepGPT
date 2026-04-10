1c31ab97bd00862eeacc4bc80ca32e9b4f55ead9
BunnySoCrazy <Master_Origami@163.com>
2026-04-10 11:12:15 +0800

    clean

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python train_LT.py -e cnnt/DeepCAD -m cnnt_vq
-CUDA_VISIBLE_DEVICES=1 python train_LT.py -e cnnt/test -m cnnt_vq
 CUDA_VISIBLE_DEVICES=0 python train_LT.py -e vhp/DeepCAD -m vhp_vq
 
 CUDA_VISIBLE_DEVICES=0 python encode_LT.py -e vhp/DeepCAD -m vhp_vq
